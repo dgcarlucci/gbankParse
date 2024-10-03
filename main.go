8884ce08dada3396fb2b719a559870740ddc93c8
Dominick Carlucci <dominick.carlucci@gmail.com>
2024-10-03 16:44:13 -0700

    cleanup

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"encoding/csv"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -14,6 +15,13 @@ import (
 	"time"
 
 	"main.go/models"
+)
+
+const (
+	clientSecret = "your client secret"
+	baseURL      = "https://us.api.blizzard.com/data/wow/item/"
+	locale       = "en_US"
+	namespace    = "static-us"
 )
 
 func main() {
@@ -63,6 +71,7 @@ func parseItem(scanner *bufio.Scanner) (models.Item, error) {
 				}
 				item.Count = count
 			}
+
 		} else if strings.HasPrefix(line, "[\"Link\"]") { //this is the singular item without info path
 			match := regexp.MustCompile(`\[\"Link\"\] = (.*?),`).FindStringSubmatch(line)
 			if match != nil {
@@ -84,55 +93,47 @@ func parseItem(scanner *bufio.Scanner) (models.Item, error) {
 }
 
 func parseInfo(scanner *bufio.Scanner) (models.Info, error) {
-	var info models.Info
+	info := models.Info{}
 	for scanner.Scan() {
 		line := scanner.Text()
-		if strings.HasPrefix(line, "[\"icon\"]") {
-			match := regexp.MustCompile(`\[\"icon\"\] = (\d+)`).FindStringSubmatch(line)
-			if match != nil {
+		switch {
+		case strings.HasPrefix(line, "[\"icon\"]"):
+			if match := regexp.MustCompile(`\[\"icon\"\] = (\d+)`).FindStringSubmatch(line); match != nil {
 				info.Icon, _ = strconv.Atoi(match[1])
 			}
-		} else if strings.HasPrefix(line, "[\"level\"]") {
-			match := regexp.MustCompile(`\[\"level\"\] = (\d+)`).FindStringSubmatch(line)
-			if match != nil {
+		case strings.HasPrefix(line, "[\"level\"]"):
+			if match := regexp.MustCompile(`\[\"level\"\] = (\d+)`).FindStringSubmatch(line); match != nil {
 				info.Level, _ = strconv.Atoi(match[1])
 			}
-		} else if strings.HasPrefix(line, "[\"rarity\"]") {
-			match := regexp.MustCompile(`\[\"rarity\"\] = (\d+)`).FindStringSubmatch(line)
-			if match != nil {
+		case strings.HasPrefix(line, "[\"rarity\"]"):
+			if match := regexp.MustCompile(`\[\"rarity\"\] = (\d+)`).FindStringSubmatch(line); match != nil {
 				info.Rarity, _ = strconv.Atoi(match[1])
 			}
-		} else if strings.HasPrefix(line, "[\"equipId\"]") {
-			match := regexp.MustCompile(`\[\"equipId\"\] = (\d+)`).FindStringSubmatch(line)
-			if match != nil {
+		case strings.HasPrefix(line, "[\"equipId\"]"):
+			if match := regexp.MustCompile(`\[\"equipId\"\] = (\d+)`).FindStringSubmatch(line); match != nil {
 				info.EquipId, _ = strconv.Atoi(match[1])
 			}
-		} else if strings.HasPrefix(line, "[\"price\"]") {
-			match := regexp.MustCompile(`\[\"price\"\] = (\d+)`).FindStringSubmatch(line)
-			if match != nil {
+		case strings.HasPrefix(line, "[\"price\"]"):
+			if match := regexp.MustCompile(`\[\"price\"\] = (\d+)`).FindStringSubmatch(line); match != nil {
 				info.Price, _ = strconv.Atoi(match[1])
 			}
-		} else if strings.HasPrefix(line, "[\"class\"]") {
-			match := regexp.MustCompile(`\[\"class\"\] = (\d+)`).FindStringSubmatch(line)
-			if match != nil {
+		case strings.HasPrefix(line, "[\"class\"]"):
+			if match := regexp.MustCompile(`\[\"class\"\] = (\d+)`).FindStringSubmatch(line); match != nil {
 				info.Class, _ = strconv.Atoi(match[1])
 			}
-		} else if strings.HasPrefix(line, "[\"subClass\"]") {
-			match := regexp.MustCompile(`\[\"subClass\"\] = (\d+)`).FindStringSubmatch(line)
-			if match != nil {
+		case strings.HasPrefix(line, "[\"subClass\"]"):
+			if match := regexp.MustCompile(`\[\"subClass\"\] = (\d+)`).FindStringSubmatch(line); match != nil {
 				info.SubClass, _ = strconv.Atoi(match[1])
 			}
-		} else if strings.HasPrefix(line, "[\"name\"]") {
-			match := regexp.MustCompile(`\[\"name\"\] = \"([^\"]+)\"`).FindStringSubmatch(line)
-			if match != nil {
+		case strings.HasPrefix(line, "[\"name\"]"):
+			if match := regexp.MustCompile(`\[\"name\"\] = \"([^\"]+)\"`).FindStringSubmatch(line); match != nil {
 				info.Name = match[1]
 			}
-		}
-		if strings.HasPrefix(line, "}") {
-			break
+		case strings.HasPrefix(line, "}"):
+			return info, nil
 		}
 	}
-	return info, nil
+	return info, fmt.Errorf("reached end of info block without closing bracket")
 }
 func parseItems(scanner *bufio.Scanner) ([]models.Item, error) {
 	items := []models.Item{}
